ddfc912c3e02d4d87252303334be0766d8538b6b
feh-franc0 <fernandofv1110@gmail.com>
2022-05-09 20:31:45 -0300

    login com node usando JWT(jsonWebToken)

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,50 @@
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const {loginValidade,registerValidade} = require('./validate');
+
+
 const userController = {
-    register:  function(req,res) {
-        console.log('register')
-        res.send('Register')
+    register: async function(req,res) {
+
+        const {error} = registerValidade(req.body)
+        if(error){return res.status(400).send(error)}
+
+        const selectUser = await User.findOne({email:req.body.email})
+        if(selectUser) return res.status(400).send('Email already exist')
+
+        const user = new User({
+            name: req.body.name,
+            email: req.body.email,
+            password: bcrypt.hashSync(req.body.password)
+        })
+
+        try {
+            const savedUser = await user.save()
+            res.send(savedUser)
+        } catch (error) {
+            res.status(400).send(error)
+        }
     },
-    login: function(req,res) {
-        console.log('login')
-        res.send('login')
+    login: async function(req,res) {
+
+        
+        const {error} = loginValidade(req.body)
+        if(error){return res.status(400).send(error)}
+
+
+        const selectUser = await User.findOne({email:req.body.email})
+        if(!selectUser) return res.status(400).send('Email or PASSWORD incorrect')
+
+        const passwordAndUserMatch = bcrypt.compareSync(req.body.password, selectUser.password)
+        if(!passwordAndUserMatch) return res.status(400).send('Email or PASSWORD incorrect')
+
+        const token = jwt.sign({_id: selectUser._id, admin: selectUser.admin},process.env.TOKEN_SECRET)
+
+        res.header('authorization-token', token)
+        res.send("User Logged")
+
     }
 }
 
